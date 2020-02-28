65c211cf2321c9870083a90aebe0a1e1e751f352
Reaveres <59661552+Reaveres@users.noreply.github.com>
2020-02-28 13:29:22 -0800

    Individual Shinies

---
+++ lab7/Shiny 1.R
@@ -0,0 +1,44 @@
+# Shiny 1 Lab 7 HW
+
+
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+
+UC_admit <- readr::read_csv("data/UC_admit.csv")
+
+UC_admit2 <- UC_admit %>% mutate(Academic_Yr = as_factor(Academic_Yr))
+
+ui <- 
+  dashboardPage(
+    dashboardHeader(title = "Plot UC by Ethnicity"),
+    dashboardSidebar(),
+    dashboardBody(
+      fluidRow(
+        box(
+          selectInput("fill", "Select Fill", choices = c("Academic_Yr", "Campus", "Category"),
+                      selected = "Academic_Yr"),
+        ), # close the first box
+        box(
+          plotOutput("plot", width = "500px", height = "400px")
+        ) # close the second box
+      ) # close the row
+    ) # close the dashboard body
+  ) # close the ui
+
+server <- function(input, output, session) { 
+  
+  # the code to make the plot of iris data grouped by species
+  output$plot <- renderPlot({
+    ggplot(UC_admit2, aes_string(x = "Ethnicity", y = "FilteredCountFR", fill = input$fill)) + 
+      geom_col(position = "dodge") + 
+      coord_flip() +
+      theme_light(base_size = 18)
+  })
+  
+  # stop the app when we close it
+  session$onSessionEnded(stopApp)
+  
+}
+
+shinyApp(ui, server)
